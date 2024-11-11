509129e7d5a684549b8db8e5bacd6e456c403268
C24405434 Cormac Barrett <C24405434@myTUDublin.ie>
2024-11-11 16:18:23 +0000

    Start of Project

---
+++ music-project/button.gd
@@ -0,0 +1,21 @@
+extends Button
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_button_down() -> void:
+	if Input.is_action_pressed("Click button"):
+		$AudioStreamPlayer2D.play()
+	pass # Replace with function body.
+
+
+func _on_audio_stream_player_2d_finished() -> void:
+	pass # Replace with function body.
