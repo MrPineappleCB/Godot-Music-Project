44a87d4b7f02f9bdd0156d4cf1649a3cd854fcaa
MrPineappleCB <c24405434@myTUDublin.ie>
2024-12-02 12:53:16 +0000

    tiles

--- music-project/thing.gd
+++ music-project/thing.gd
@@ -1,4 +1,5 @@
 extends Node2D
+var s = 0
 
 
 # Called when the node enters the scene tree for the first time.
@@ -23,19 +24,23 @@ func _draw() -> void:
 """
 
 func _draw() -> void:
-	print("ver")
+	#print("ver")
 	var r = get_viewport_rect()
 	r.size.x = r.size.x / 8
 	r.size.y = r.size.y / 8
 	var tile0 = Rect2(0,0, r.size.x, r.size.y)
 	var tile1 = Rect2(r.size.x,0, r.size.x, r.size.y)
-	var tiles = [tile0, tile1]
-	for tile in range(len(tiles)):
-		print("hune")
+	var tile2 = Rect2(r.size.x, r.size.y, r.size.x, r.size.y)
+	var tiles = [tile0, tile1, tile2]
+	#print("hune")
 		#await Input.is_action_just_pressed("test")
-		if $Button.button_down:
-			print("piss")
-			draw_rect(tiles[tile], Color.RED)
+	if Input.is_action_just_pressed("Click button"):
+		draw_rect(tiles[s], Color.RED)
+		print(s)
+		$AudioStreamPlayer2D.play()
+		s+=1
+		#print(s)
+		queue_redraw()
 		#print("reach here")
 	#print("reach")
 	pass
@@ -51,6 +56,10 @@ func _on_timer_timeout() -> void:
 	pass # Replace with function body.
 
 
-func _on_button_button_down() -> void:
-	_draw()
+#func _on_button_button_down() -> void:
+	#_draw()
+	#pass # Replace with function body.
+
+
+func _on_audio_stream_player_2d_finished() -> void:
 	pass # Replace with function body.
